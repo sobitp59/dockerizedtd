f84f8674b4de5f8cd5e947d0bd9ef83b7d387fc8
sobitp59 <sobitp59@gmail.com>
2025-02-28 11:33:51 +0530

    test

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: 'export',
+//   basePath: '/dockerizedtd',
+// };
+
+// export default nextConfig;
+module.exports = {
+  output: "export",
+};
